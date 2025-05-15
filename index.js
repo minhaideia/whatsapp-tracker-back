18f55599404cc46ecec777ae0f64601eb62eac5f
Pedro Tardivo <data.tardivo@gmail.com>
2025-05-15 19:18:55 -0300

    Fix: Middleware CORS universal garantindo CORS em todas as respostas

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const fs = require('fs-extra');
 
 const app = express();
 const cors = require('cors');
+
+// Universal CORS middleware
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.use(cors());
 app.use(express.json());
 
